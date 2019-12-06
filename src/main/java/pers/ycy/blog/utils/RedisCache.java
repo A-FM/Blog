b594d77c536f03ae33e480c16a22c8fa0ca946db
x-power <1376918818@qq.com>
2019-12-06 13:28:03 +0800

    1: Redis工具类

--- src/main/java/pers/ycy/blog/utils/RedisCache.java
+++ src/main/java/pers/ycy/blog/utils/RedisCache.java
@@ -1,133 +1,101 @@
 package pers.ycy.blog.utils;
 
-import org.apache.ibatis.cache.Cache;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-@Component
-public class RedisCache implements Cache {
-    private static final Logger logger = LoggerFactory.getLogger(RedisCache.class);
+public class RedisCache {
 
-    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-    private final String id; // cache instance id
-    private RedisTemplate redisTemplate;
+    @Autowired
+    private static RedisTemplate<String,Object> redisTemplate;
 
-    private static final long EXPIRE_TIME_IN_MINUTES = 30; // redis过期时间
 
-    public RedisCache() {
-        id = null;
-    }
-
-    public RedisCache(String id) {
-        if (id == null) {
-            throw new IllegalArgumentException("Cache instances require an ID");
-        }
-        this.id = id;
-    }
-
-    @Override
-    public String getId() {
-        return id;
+    /**
+     * 带有过期时间的键值对设置
+     * @param key 键
+     * @param value 值
+     * @param second 过期时间(/SECONDS)
+     */
+    public static void put(String key, Object value, long second) {
+        redisTemplate.opsForValue().set(key,value,second,TimeUnit.SECONDS);
     }
 
     /**
-     * Put query result to redis
-     *
-     * @param key
-     * @param value
+     * 获取对应键的值
+     * @param key 键
+     * @return Object对象
      */
-    @Override
-    public void putObject(Object key, Object value) {
-        try {
-            RedisTemplate redisTemplate = getRedisTemplate();
-            ValueOperations opsForValue = redisTemplate.opsForValue();
-            opsForValue.set(key, value, EXPIRE_TIME_IN_MINUTES, TimeUnit.MINUTES);
-            logger.debug("Put query result to redis");
-        } catch (Throwable t) {
-            logger.error("Redis put failed", t);
-        }
+    public static Object get(String key) {
+
+        return redisTemplate.opsForValue().get(key);
     }
 
     /**
-     * Get cached query result from redis
-     *
-     * @param key
-     * @return
+     * 获取当前键的过期时间
+     * @param key 键
+     * @return 过期时间(/SECONDS)
      */
-    @Override
-    public Object getObject(Object key) {
-        try {
-            RedisTemplate redisTemplate = getRedisTemplate();
-            ValueOperations opsForValue = redisTemplate.opsForValue();
-            logger.debug("Get cached query result from redis");
-//            System.out.println("****" + opsForValue.get(key).toString());
-            return opsForValue.get(key);
-        } catch (Throwable t) {
-            logger.error("Redis get failed, fail over to db", t);
-            return null;
-        }
+    public static Long getExpire(String key){
+        return redisTemplate.getExpire(key);
     }
 
     /**
-     * Remove cached query result from redis
-     *
-     * @param key
-     * @return
+     * 判断键是否存在
+     * @param key 键
+     * @return 是否存在
      */
-    @Override
-    @SuppressWarnings("unchecked")
-    public Object removeObject(Object key) {
-        try {
-            RedisTemplate redisTemplate = getRedisTemplate();
-            redisTemplate.delete(key);
-            logger.debug("Remove cached query result from redis");
-        } catch (Throwable t) {
-            logger.error("Redis remove failed", t);
-        }
-        return null;
+    public static Boolean hasKey(String key){
+        return redisTemplate.hasKey(key);
     }
 
     /**
-     * Clears this cache instance
+     * 设置当前键的过期时间
+     * @param key 键
+     * @param timeout 过期时间
+     * @param unit 单位
+     * @return 是否成功
      */
-    @Override
-    public void clear() {
-        RedisTemplate redisTemplate = getRedisTemplate();
-        redisTemplate.execute((RedisCallback) connection -> {
-            connection.flushDb();
-            return null;
-        });
-        logger.debug("Clear all the cached query result from redis");
+    public static Boolean expire(String key,Long timeout,TimeUnit unit){
+        return redisTemplate.expire(key,timeout,unit);
     }
 
     /**
-     * This method is not used
-     *
-     * @return
+     * 设置当前键在 Date时 过期
+     * @param key 键
+     * @param date Date 类
+     * @return 是否成功
      */
-    @Override
-    public int getSize() {
-        return 0;
+    public static Boolean expireAt(String key, Date date){
+        return redisTemplate.expireAt(key,date);
     }
 
-    @Override
-    public ReadWriteLock getReadWriteLock() {
-        return readWriteLock;
+    /**
+     * 移除当前键的过期时间, 持久存储该键值对.
+     * @param key 键
+     * @return 是否成功
+     */
+    public static Boolean persist(String key){
+        return redisTemplate.persist(key);
     }
 
-    private RedisTemplate getRedisTemplate() {
-        if (redisTemplate == null) {
-            redisTemplate = ApplicationContextHolder.getBean("redisTemplate");
-        }
-        return redisTemplate;
+    /**
+     * 删除给定的 {@code key}
+     * @param key 键
+     * @return 是否成功
+     */
+    public static Boolean delete(String key){
+        return redisTemplate.delete(key);
+    }
+
+    /**
+     * 重命名key键, 只有当 {@code newKey} 不存在的时候.
+     * @param oldKey 旧键
+     * @param newKey 新键
+     * @return 是否成功
+     */
+    public static Boolean renameIfAbsent(String oldKey,String newKey){
+        return redisTemplate.renameIfAbsent(oldKey,newKey);
     }
 }
